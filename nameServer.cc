847b87cbd3cd9b92f0d3be1a2f130dc62e99d198
jaskirt <jaskirt.pooni@gmail.com>
2015-12-05 02:56:53 -0500

    Finished nameServer

--- nameServer.cc
+++ nameServer.cc
@@ -1,17 +1,36 @@
 #include "nameServer.h"
 
+void NameServer::main() {
+	int vm_id = 0;
+	for (int i = 0; i < numStudents; i++) {
+		students[i] = vm_id;
+		vm_id = (vm_id + 1) % numVendingMachines; 
+	}
+}
+
 NameServer::NameServer(Printer &prt, unsigned int numVendingMachines, unsigned int numStudents): prt(prt), 
 																								 numVendingMachines(numVendingMachines),
-																								 numStudents(numStudents) {};
+																								 numStudents(numStudents),
+																								 registered(0) {
+	machineList = new VendingMachine*[numVendingMachines];
+	students = new unsigned int[numStudents];
+}
+
+NameServer::~NameServer() {
+	delete machineList;
+	delete studetns;
+}
 
 void NameServer::VMregister(VendingMachine *vendingmachine) {
-
+	machineList[registered] = vendingmachine;
+	registered++;
 }
 
 VendingMachine *NameServer::getMachine(unsigned int id) {
-
+	VendingMachine *vm = machineList[students[i]];
+	students[i] = (students[i] + 1) % numVendingMachines;   
 }
 
 VendingMachine **NameServer::getMachineList() {
-	
+	return machineList;
 }
--- nameServer.h
+++ nameServer.h
@@ -1,12 +1,17 @@
 #pragma once
+#include "VendingMachine.h"
 
 _Task NameServer {
 	Printer &prt;
 	unsigned int numVendingMachines;
 	unsigned int numStudents;
+	VendingMachine **machineList;
+	unsigned int *students;
+	unsigned int registered;
     void main();
   public:
     NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents );
+    ~NameServer();
     void VMregister( VendingMachine *vendingmachine );
     VendingMachine *getMachine( unsigned int id );
     VendingMachine **getMachineList();
