4ec97d556747fbfa0eca7b58753ca5ff3fb98f0a
Digvijay Makwana <d2makwan@uwaterloo.ca>
2015-12-05 02:14:22 -0500

    parent module draft finished

--- parent.cc
+++ parent.cc
@@ -2,14 +2,17 @@
 #include "bank.h"
 #include "MPRNG.h"
 
+extern MPRNG mprng;
+
 Parent::main() {
 	for (;;) {
 		_Accept(~Parent) {
-
-		} or _Accept() {
-
-		} _Else {
-
+			break;
+		} _Else {					// busy-waiting for call to its destructor
+			unsigned int randStudent = mprng(numStudents-1);
+			unsigned int randAmount = mprng(1,3);
+			yield(parentalDelay);
+			bank.deposit(randStudent, randAmount);
 		}
 	}
 }
