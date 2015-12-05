745046b152891b5cf15cfe2aabdb4a413b9b0b9e
jaskirt <jaskirt.pooni@gmail.com>
2015-12-05 03:34:40 -0500

    Added truck.cc

--- truck.h
+++ truck.h
@@ -1,6 +1,14 @@
 #pragma once
+#include "printer.h"
+#include "nameServer.h"
+#include "bottlingPlant.h"
 
 _Task Truck {
+	Printer &prt;
+	NameServer &nameServer;
+	BottlingPlant &plant;
+	unsigned int numVendingMachines;
+	unsigned int maxStockPerFlavour;
     void main();
   public:
     Truck( Printer &prt, NameServer &nameServer, BottlingPlant &plant,
