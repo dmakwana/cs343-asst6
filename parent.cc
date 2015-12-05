e5b8dbcc1402493714975d978a1834418495c522
Digvijay Makwana <d2makwan@uwaterloo.ca>
2015-12-05 02:04:23 -0500

    adding parent module code

---
+++ parent.cc
@@ -0,0 +1,18 @@
+#include "printer.h"
+#include "bank.h"
+#include "MPRNG.h"
+
+Parent::main() {
+	for (;;) {
+		_Accept(~Parent) {
+
+		} or _Accept() {
+
+		} _Else {
+
+		}
+	}
+}
+
+Parent::Parent(Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay) :
+	prt(prt), bank(bank), numStudents(numStudents), parentalDelay(parentalDelay) {}
--- parent.h
+++ parent.h
@@ -1,7 +1,11 @@
 #pragma once
 
 _Task Parent {
+	Printer &prt;
+	Bank &bank;
+	unsigned int numStudents;
+	unsigned int parentalDelay;
     void main();
   public:
-    Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay );
+    Parent(Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay);
 };
