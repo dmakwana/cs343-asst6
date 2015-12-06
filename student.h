8cdbdddd4f1075a3b7bb99a6dd42c512ccb9032b
jaskirt <jaskirt.pooni@gmail.com>
2015-12-05 19:26:46 -0500

    Added student.cc

--- student.h
+++ student.h
@@ -1,6 +1,12 @@
 #pragma once
 
 _Task Student {
+	Printer &prt;
+	NameServer &nameServer;
+	WATCardOffice &cardOffice;
+	Groupoff &groupoff;
+    unsigned int id;
+    unsigned int maxPurchases
     void main();
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, Groupoff &groupoff,
