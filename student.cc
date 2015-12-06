2942af657755e1c177d8b5a8695d889c31818cce
jaskirt <jaskirt.pooni@gmail.com>
2015-12-06 16:09:23 -0500

    Added prints to student

--- student.cc
+++ student.cc
@@ -6,6 +6,7 @@
 extern MPRNG mprng;
 
 void Student::main() {
+	prt.print(Printer::Student, 'S', favorite, num_sodas);
 	for (unsigned int i = 0; i < num_sodas;) {
 		yield(mprng(1,10));
 		while(true) {
@@ -13,7 +14,9 @@ void Student::main() {
 				if(fcard.available()) {
 					try {
 						vm->buy(favorite, *fcard());
+						prt.print(Printer::Student, 'B', giftCard()->getBalance());
 					} catch (WATCardOffice::Lost l) {
+						prt.print(Printer::Student, 'L');
 						fcard = cardOffice.create(id, 5);
 						continue;
 					} catch (VendingMachine::Funds f) {
@@ -23,16 +26,19 @@ void Student::main() {
 				}
 				else if(giftCard.available()) {
 					vm->buy(favorite, *giftCard());
+					prt.print(Printer::Student, 'G', giftCard()->getBalance());
 					giftCard.reset();
 				}
 			} catch (VendingMachine::Stock s) {
 				vm = nameServer.getMachine(id);
+				prt.print(Printer::Student, 'V', vm->getId());
 				break;
 			}
 			i++;
 			break;
 		}
 	}
+	prt.print(Printer::Student, 'F');
 }
 
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, Groupoff &groupoff,
