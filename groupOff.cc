8a6765df173781d18f2dc98f751b1b5541cf1d1f
jaskirt <jaskirt.pooni@gmail.com>
2015-12-06 01:18:36 -0500

    Added groupOff

--- groupOff.cc
+++ groupOff.cc
@@ -1,16 +1,47 @@
 #include "groupOff.h"
+#include "MPRNG.h"
+
+extern MPRNG mprng;
 
 void Groupoff::main() {
+	for (unsigned int i = 0; i < numStudents; i++) {
+		_Accept(giftCard);
+	}
+	while (true) {
+		_Accept(~Groupoff) {
+			break;
+		} _Else {
+			yield(groupoffDelay);
+			if (giftcard_id.empty()) {
+				break;
+			}
+			unsigned int id = mprng(giftcard_id.size());
+			giftcard_id.erase(giftcard_id.begin() + id);
+			giftCards[id].delivery(&watCards[id]);
+		}
 	
+	}
 }
-
+	
 Groupoff::Groupoff( Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay ): prt(prt),
 																												 numStudents(numStudents),
 																												 sodaCost(sodaCost),
-																												 groupoffDelay(groupoffDelay) {
-
+																												 groupoffDelay(groupoffDelay),
+																												 currCard(0) {
+	giftCards = new WATCard::FWATCard[numStudents];
+	watCards = new WATCard[numStudents];
+	for (unsigned int i = 0; i < numStudents; i++) {
+		watCards[i].deposit(sodaCost);
+		giftcard_id.push_back(i);
+	}
 }
 
-WATCard::FWATCard Groupoff::giftCard(){
-	return WATCard::FWATCard();
+Groupoff::~Groupoff() {
+	delete[] giftCards;
+	delete[] watCards;
+}
+
+WATCard::FWATCard Groupoff::giftCard() {
+	currCard++;
+	return giftCards[currCard - 1];
 }
--- groupOff.h
+++ groupOff.h
@@ -1,14 +1,20 @@
 #pragma once
 #include "printer.h"
 #include "watcard.h"
+#include <vector>
 
 _Task Groupoff {
 	Printer &prt;
+	WATCard::FWATCard *giftCards;
+	WATCard *watCards;
+	std::vector<unsigned int> giftcard_id;
 	unsigned int numStudents;
 	unsigned int sodaCost;
 	unsigned int groupoffDelay; 
+	unsigned int currCard;
     void main();
   public:
     Groupoff( Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay );
+    ~Groupoff();
     WATCard::FWATCard giftCard();
 };
