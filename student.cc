f53867154337d230d4f9db87080a7d14f2879020
jaskirt <jaskirt.pooni@gmail.com>
2015-12-06 15:49:13 -0500

    Added student.cc

--- student.cc
+++ student.cc
@@ -1,9 +1,48 @@
 #include "student.h"
+#include "vendingMachine.h"
+#include "groupOff.h"
+#include "MPRNG.h"
+
+extern MPRNG mprng;
 
 void Student::main() {
-	
+	for (unsigned int i = 0; i < num_sodas;) {
+		yield(mprng(1,10));
+		while(true) {
+			try {
+				if(fcard.available()) {
+					try {
+						vm->buy(favorite, *fcard());
+					} catch (WATCardOffice::Lost l) {
+						fcard = cardOffice.create(id, 5);
+						continue;
+					} catch (VendingMachine::Funds f) {
+						fcard = cardOffice.transfer(id,  vm->cost() + 5, fcard());
+						break;
+					}
+				}
+				else if(giftCard.available()) {
+					vm->buy(favorite, *giftCard());
+					giftCard.reset();
+				}
+			} catch (VendingMachine::Stock s) {
+				vm = nameServer.getMachine(id);
+				break;
+			}
+			i++;
+			break;
+		}
+	}
 }
 
 Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, Groupoff &groupoff,
          unsigned int id, unsigned int maxPurchases ): prt(prt), nameServer(nameServer), cardOffice(cardOffice),
-         											   groupoff(groupoff), id(id), maxPurchases() {}
+         											   groupoff(groupoff), id(id), maxPurchases(maxPurchases) {
+    
+    num_sodas = mprng(1, maxPurchases);
+    favorite = static_cast<VendingMachine::Flavours>(mprng(VendingMachine::NUM_FLAVOURS-1));
+    fcard = cardOffice.create(id, 5);
+    giftCard = groupoff.giftCard();
+    vm = nameServer.getMachine(id);
+
+}
--- student.h
+++ student.h
@@ -3,15 +3,21 @@
 #include "nameServer.h"
 #include "watcardOffice.h"
 #include "groupOff.h"
+#include "watcard.h"
+#include "vendingMachine.h"
 
 _Task Student {
 	Printer &prt;
 	NameServer &nameServer;
 	WATCardOffice &cardOffice;
 	Groupoff &groupoff;
-    unsigned int id;
-    unsigned int maxPurchases;
-    void main();
+  unsigned int id;
+  unsigned int maxPurchases;
+  void main();
+  unsigned int num_sodas;
+  VendingMachine::Flavours favorite;
+  VendingMachine *vm;
+  WATCard::FWATCard fcard, giftCard;
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, Groupoff &groupoff,
              unsigned int id, unsigned int maxPurchases );
