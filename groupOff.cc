6477816cf11982504c0626766689adfdaee1ecde
jaskirt <jaskirt.pooni@gmail.com>
2015-12-05 19:09:45 -0500

    Added groupOff.cc

---
+++ groupOff.cc
@@ -0,0 +1,12 @@
+#include "groupOff.h"
+
+Groupoff::Groupoff( Printer &prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay ): prt(prt),
+																												 numStudents(numStudents),
+																												 sodaCost(sodaCost),
+																												 groupoffDelay(groupoffDelay) {
+
+}
+
+WATCard::FWATCard Groupoff::giftCard(){
+	return WATCard::FWATCard();
+}
