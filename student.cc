f8828fdcbd36d30992104140eb3bb7ab69d87d83
jaskirt <jaskirt.pooni@gmail.com>
2015-12-06 20:43:24 -0500

    Fixed student

--- student.cc
+++ student.cc
@@ -10,7 +10,6 @@ extern MPRNG mprng;
 using namespace std;
 
 void Student::main() {
-	prt.print(Printer::Student, id, 'S', favorite, num_sodas);
     prt.debug("student7"  );
 	for (unsigned int i = 0; i < num_sodas;) {
     	prt.debug("student8"  );
@@ -23,6 +22,8 @@ void Student::main() {
     					prt.debug("student9"  );
 						vm->buy(favorite, *fcard());
 						prt.print(Printer::Student, id, 'B', giftCard()->getBalance());
+						i++;
+						break;
 					} catch (WATCardOffice::Lost l) {
 						prt.print(Printer::Student, id, 'L');
 						fcard = cardOffice.create(id, 5);
@@ -39,6 +40,8 @@ void Student::main() {
 					prt.print(Printer::Student, id, 'G', giftCard()->getBalance());
 					prt.debug("student13"  );
 					giftCard.reset();
+					i++;
+					break;
 				}
 				prt.debug("student11"  );
 			} catch (VendingMachine::Stock s) {
@@ -46,8 +49,7 @@ void Student::main() {
 				prt.print(Printer::Student, id, 'V', vm->getId());
 				break;
 			}
-			i++;
-			break;
+			
 		}
 	}
 	prt.print(Printer::Student, id, 'F');
@@ -67,6 +69,8 @@ Student::Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffic
     giftCard = groupoff.giftCard();
     prt.debug("student5"  );
     vm = nameServer.getMachine(id);
+    prt.print(Printer::Student, id, 'S', favorite, num_sodas);
+    prt.print(Printer::Student, id, 'V', vm->getId());
     prt.debug("student6"  );
 
 }
